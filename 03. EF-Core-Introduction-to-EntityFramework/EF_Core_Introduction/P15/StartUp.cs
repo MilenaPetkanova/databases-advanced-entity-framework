08f578ec3687fb75c5927c4050be4a68cd38cbc2
Milena Petkanova <milenaslaveikova78@gmail.com>
2018-07-07 11:47:38 +0300

    add exercise P15

--- 03. EF-Core-Introduction-to-EntityFramework/EF_Core_Introduction/P15/StartUp.cs
+++ 03. EF-Core-Introduction-to-EntityFramework/EF_Core_Introduction/P15/StartUp.cs
@@ -3,7 +3,7 @@
 namespace P15
 {
     using P02_DatabaseFirst.Data;
-    using System.IO;
+    using System;
     using System.Linq;
 
     public class StartUp
@@ -12,10 +12,34 @@ namespace P15
         {
             using (var context = new SoftUniContext())
             {
-                using (StreamWriter sw = new StreamWriter("../Employees.txt"))
-                {
+                var townName = Console.ReadLine();
 
+                var adressesInTown = context.Addresses
+                    .Where(a => a.Town.Name.Equals(townName)).ToArray();
+
+                var deletedAddresses = 0;
+
+                foreach (var address in adressesInTown)
+                {
+                    var employeesOnAddress = context.Employees
+                        .Where(e => e.Address.AddressId == address.AddressId);
+
+                    foreach (var employee in employeesOnAddress)
+                    {
+                        employee.AddressId = null;
+                    }
+
+                    context.Addresses.Remove(address);
+                    deletedAddresses++;
                 }
+
+                var town = context.Towns.FirstOrDefault(t => t.Name.Equals(townName));
+
+                context.Towns.Remove(town);
+
+                context.SaveChanges();
+
+                Console.WriteLine($"{deletedAddresses} address in {townName} was deleted");
             }
         }
     }
